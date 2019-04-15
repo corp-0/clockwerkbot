eebfb73770a69fed280f94412fd67c9b7cd8fd37
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-04-15 08:37:29 -0400

    chore: add better documentation in the code

--- clockwerkbot.py
+++ clockwerkbot.py
@@ -21,15 +21,39 @@ from discord.ext import commands
 
 # global variables
 DEBUG = True
-clockwerk     = commands.Bot(command_prefix='/cw ', case_insensitive=True) # Dándole prefijo a sus comandos
-game          = discord.Game('Dota 2') # Qué juego juega el bot
-sepa          = "************************************"  # Un separador para la consola
-with open('respuestas.json', 'r') as array: # frases de clockwerk
+clockwerk     = commands.Bot(command_prefix='/cw ', case_insensitive=True) # Prefixes for the commands
+game          = discord.Game('Dota 2') # What game is the bot playing
+sepa          = "************************************"  # just decor
+with open('respuestas.json', 'r') as array: # spawning/dying dialogue lines
     data = json.load(array)
+
+# funciones
+def broodmama(my_url):
+    """Spider that crawls the internet. Returns the entire webpage that needs to be parsed.
+    Args: 
+        0 -- my_url <STRING> - The webpage broodmoma needs to crawl."""
+
+    try:
+        headers  = {}
+        headers['User-Agent'] =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+        req      = urllib.request.Request(my_url, headers=headers)
+        resp     = spyder(req)
+        respData = resp.read()
+        resp.close()
+        page_soup  = soup(respData, 'html.parser')
+        return page_soup
+
+    except Exception as e:
+        print('Error trying to crawl: '+ str(e))
+        return False
 
 # events
 @clockwerk.event
-async def on_ready(): # Al estar listo el bot
+async def on_ready():
+    """This event ocurrs whenever the bot is online. 
+    As soon as he gets only, he will try to do a 'spawn' dialogue line in the
+    channel called 'dota2'. If there isn't one, then your server is not that cool."""
+
     print('El bot ha iniciado sesión')
     if (clockwerk.is_ready()):
         print(sepa)
@@ -51,9 +75,14 @@ async def on_ready(): # Al estar listo el bot
             pass
 
 # Commands
-@clockwerk.command() # Devuelve los 3 mejores y el peor héroe del tiempo solicitado, según dotabuff
+@clockwerk.command()
 async def op(ctx, tiempo = None, hero = None ):
-    # La araña
+    """Inquires the bots with the best 3 heroes from the period of time defined in the arguments.
+    Args:
+        0 -- ctx <OBJECT> - channel from where the bot was called.
+        1 -- tiempo <STRING> - the period of time to ask the bot.
+        2 -- hero <STRING> - Optional, the specific hero you want to know about."""
+
     if tiempo == None:
         await ctx.send('El tiempo es un argumento requerido')
         await ctx.send('Posibles valores: genesis, mes, semana')
@@ -74,14 +103,11 @@ async def op(ctx, tiempo = None, hero = None ):
         await ctx.send('Posibles valores: genesis, mes, semana')
         return False
 
-    try:
-        headers  = {}
-        headers['User-Agent'] =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-        req      = urllib.request.Request(my_url, headers=headers)
-        resp     = spyder(req)
-        respData = resp.read()
-        resp.close()
-        page_soup  = soup(respData, 'html.parser')
+    #Llamando la arañita
+    page_soup = broodmama(my_url)
+
+    try:    
+        #Parseando resultados
         valor = []
         for tr in page_soup.find_all('tr'):
             valores = [td.text for td in tr.find_all('td')]
@@ -137,6 +163,12 @@ async def op(ctx, tiempo = None, hero = None ):
 
 @clockwerk.command()
 async def say(ctx, msg: str, cnl = None):
+    """Makes the bot say whatever you want in whatever channel you want.
+    Args:
+        0 -- ctx <OBJECT> - Channel from where the bot is called.
+        1 -- msg <STRING> - The message to say.
+        2 -- cnl <STRING> - Optional, specified channel to say the message."""
+
     if cnl == None:
         canal = ctx
         await ctx.send(msg)
@@ -149,6 +181,12 @@ async def say(ctx, msg: str, cnl = None):
 
 @clockwerk.command()
 async def sayAloud(ctx, msg: str, cnl = None):
+    """Makes the bot say whatever you want in whatever channel you want with TTS.
+    Args:
+        0 -- ctx <OBJECT> - Channel from where the bot is called.
+        1 -- msg <STRING> - The message to say.
+        2 -- cnl <STRING> - Optional, specified channel to say the message."""
+
     if cnl == None:
         canal = ctx
         await ctx.send(msg, tts=True)
@@ -159,8 +197,10 @@ async def sayAloud(ctx, msg: str, cnl = None):
         else:
             await canal.send(msg, tts=True)
     
-@clockwerk.command() # Mata al robot, no le duele
+@clockwerk.command()
 async def shutdown(ctx):
+    """Kills the robot, it doesn't hurt tho."""
+
     await ctx.send(random.choice(data['dying']))
     await clockwerk.logout()
   
